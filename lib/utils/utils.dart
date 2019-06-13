9167f96c53b57c5cae2b2449e88fd359f3b602a5
Masayuki Ono <mono0926@gmail.com>
2019-06-13 14:35:12 +0900

    Fix import

--- lib/utils/utils.dart
+++ lib/utils/utils.dart
@@ -1,5 +1,4 @@
 export 'package:mono_kit/functions/functions.dart';
-export 'package:mono_kit/plugins/ios_info_plist_reader.dart';
 
 export 'better_cached_network_image_provider.dart';
 export 'hud.dart';
