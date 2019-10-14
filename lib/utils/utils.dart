65d24b57ce6849cff5de45d8d24989587e634d16
Masayuki Ono <mono0926@gmail.com>
2019-10-14 10:45:04 +0900

    Move subscription_holder

--- lib/utils/utils.dart
+++ lib/utils/utils.dart
@@ -1,9 +1,9 @@
 export 'package:mono_kit/functions/functions.dart';
+export 'package:subscription_holder/subscription_holder.dart';
 
 export 'better_cached_network_image_provider.dart';
 export 'build_mode.dart';
 export 'hud.dart';
 export 'life_cycle_observer.dart';
 export 'progress_mixin.dart';
-export 'subscription_holder.dart';
 export 'text_answer_dialog.dart';
