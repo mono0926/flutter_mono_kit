60ada56fa48bd8d53d7b949bb160340d27a27614
Masayuki Ono <mono0926@gmail.com>
2020-02-03 22:03:16 +0900

    Delete Typography.material2018()

--- lib/extensions/theme_data.dart
+++ lib/extensions/theme_data.dart
@@ -4,7 +4,6 @@ extension ThemeDataEx on ThemeData {
   /// Create ThemeData which follows latest Material Design Spec.
   ThemeData followLatestSpec() {
     return copyWith(
-      typography: Typography.material2018(),
       snackBarTheme: snackBarTheme.copyWith(
         behavior: SnackBarBehavior.floating,
       ),
