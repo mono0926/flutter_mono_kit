9dea19d81082a49d88fbe61e506ff333129439f6
Masayuki Ono <mono0926@gmail.com>
2020-02-03 21:59:31 +0900

    Change to 0.13.6+1

--- lib/extensions/theme_data.dart
+++ lib/extensions/theme_data.dart
@@ -4,22 +4,11 @@ extension ThemeDataEx on ThemeData {
   /// Create ThemeData which follows latest Material Design Spec.
   ThemeData followLatestSpec() {
     return copyWith(
-      typography: Typography.material2018().copyWith(
-        englishLike: material2018EnglishLikeFixed(),
-      ),
+      typography: Typography.material2018(),
       snackBarTheme: snackBarTheme.copyWith(
         behavior: SnackBarBehavior.floating,
       ),
       splashFactory: InkRipple.splashFactory,
     );
   }
-}
-
-// Workaround: https://github.com/flutter/flutter/pull/49756
-TextTheme material2018EnglishLikeFixed() {
-  final englishLike = Typography.material2018().englishLike;
-  return englishLike.copyWith(
-    bodyText1: englishLike.bodyText2,
-    bodyText2: englishLike.bodyText1,
-  );
 }
