c38edc3e43189d43a8a9e92f542d54604036a1b6
Masayuki Ono <mono0926@gmail.com>
2020-02-03 22:05:08 +0900

    Revert "Delete Typography.material2018()"
    This reverts commit 60ada56fa48bd8d53d7b949bb160340d27a27614.

--- lib/extensions/theme_data.dart
+++ lib/extensions/theme_data.dart
@@ -4,6 +4,7 @@ extension ThemeDataEx on ThemeData {
   /// Create ThemeData which follows latest Material Design Spec.
   ThemeData followLatestSpec() {
     return copyWith(
+      typography: Typography.material2018(),
       snackBarTheme: snackBarTheme.copyWith(
         behavior: SnackBarBehavior.floating,
       ),
