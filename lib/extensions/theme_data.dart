daaa86a6826c3db5cbef3dc35d2245af33a2fede
Masayuki Ono <mono0926@gmail.com>
2020-02-03 22:05:20 +0900

    Revert "Change to 0.13.6+1"
    This reverts commit 9dea19d81082a49d88fbe61e506ff333129439f6.

--- lib/extensions/theme_data.dart
+++ lib/extensions/theme_data.dart
@@ -4,11 +4,22 @@ extension ThemeDataEx on ThemeData {
   /// Create ThemeData which follows latest Material Design Spec.
   ThemeData followLatestSpec() {
     return copyWith(
-      typography: Typography.material2018(),
+      typography: Typography.material2018().copyWith(
+        englishLike: material2018EnglishLikeFixed(),
+      ),
       snackBarTheme: snackBarTheme.copyWith(
         behavior: SnackBarBehavior.floating,
       ),
       splashFactory: InkRipple.splashFactory,
     );
   }
+}
+
+// Workaround: https://github.com/flutter/flutter/pull/49756
+TextTheme material2018EnglishLikeFixed() {
+  final englishLike = Typography.material2018().englishLike;
+  return englishLike.copyWith(
+    bodyText1: englishLike.bodyText2,
+    bodyText2: englishLike.bodyText1,
+  );
 }
