3cca66f6a8da807d91c50f82123ea5d1e2e35925
Masayuki Ono <mono0926@gmail.com>
2020-02-17 17:41:38 +0900

    Fix svg

--- lib/widgets/app_asset_image.dart
+++ lib/widgets/app_asset_image.dart
@@ -1,4 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+
+enum AppAssetImageType {
+  png,
+  svg,
+}
 
 class AppAssetImage extends StatelessWidget {
   const AppAssetImage({
@@ -6,21 +12,46 @@ class AppAssetImage extends StatelessWidget {
     @required this.name,
     this.fit,
     this.scale = 3,
-    this.extension = 'png',
+    this.type = AppAssetImageType.png,
   }) : super(key: key);
 
   final String name;
   final BoxFit fit;
   final double scale;
-  final String extension;
+  final AppAssetImageType type;
 
-  Image get _widget => Image.asset(
-        'assets/images/$name.$extension',
+  Widget get _widget {
+    switch (type) {
+      case AppAssetImageType.png:
+        return assetImage;
+      case AppAssetImageType.svg:
+        return svgPicture;
+    }
+    assert(false, 'Unexpected type: $type');
+    return null;
+  }
+
+  Image get assetImage => Image.asset(
+        'assets/images/$name.png',
         scale: scale,
         fit: fit,
       );
 
-  ImageProvider get image => _widget.image;
+  SvgPicture get svgPicture => SvgPicture.asset(
+        'assets/images/$name.svg',
+        fit: fit,
+      );
+
+  ImageProvider get image {
+    switch (type) {
+      case AppAssetImageType.png:
+        return assetImage.image;
+      case AppAssetImageType.svg:
+        return null;
+    }
+    assert(false, 'Unexpected type: $type');
+    return null;
+  }
 
   @override
   Widget build(BuildContext context) {
