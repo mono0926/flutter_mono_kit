29fca1655ac4bec55f4af20e30b6aa0f008d7369
Masayuki Ono <mono0926@gmail.com>
2020-03-21 13:55:42 +0900

    Delete confirm_dialog

--- lib/functions/functions.dart
+++ lib/functions/functions.dart
@@ -1,9 +1,1 @@
-import 'dart:io';
-
-import 'package:flutter/foundation.dart';
-
-export 'confirm_dialog.dart';
-export 'modal_bottom_sheet.dart';
 export 'show_error_dialog.dart';
-
-final bool isCupertinoStyle = !kIsWeb && Platform.isIOS;
