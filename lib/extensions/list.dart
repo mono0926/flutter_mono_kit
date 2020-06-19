3eb480988b6b3012f58cdae62fd7fd966220617d
Masayuki Ono <mono0926@gmail.com>
2020-06-19 13:16:06 +0900

    Omit parameter name

--- lib/extensions/list.dart
+++ lib/extensions/list.dart
@@ -1,5 +1,5 @@
 extension ListEx<T> on List<T> {
-  List<E> mapWithIndex<E>(E Function(T item, int index) f) => asMap()
+  List<E> mapWithIndex<E>(E Function(T, int index) f) => asMap()
       .entries
       .map((entry) => f(
             entry.value,
