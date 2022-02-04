e7405fea5b6f521f100cfe1c58086d771d13ae53
Masayuki Ono <mono0926@gmail.com>
2022-02-04 16:24:01 +0900

    Fix barrier duration

--- example/lib/pages/barrier_page.dart
+++ example/lib/pages/barrier_page.dart
@@ -28,7 +28,7 @@ class _BarrierPageState extends ConsumerState<BarrierPage> {
           // このブロックで囲まれている処理が終わるまでプログレス表示
           ref.read(barrierProvider.notifier).executeWithProgress(() async {
             // ダミー処理時間として1秒待つ
-            await Future<void>.delayed(const Duration(seconds: 5));
+            await Future<void>.delayed(const Duration(seconds: 1));
             setState(() => _count++);
           });
         },
