b39daa4ff4f628bff763ce7914cc24775e898290
mono <mono0926@gmail.com>
2022-07-08 14:39:39 +0900

    Delete # from URL

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,10 +1,12 @@
 import 'package:flutter/material.dart' hide Router;
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_web_plugins/url_strategy.dart';
 
 import 'app.dart';
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
+  setUrlStrategy(PathUrlStrategy());
   runApp(
     const ProviderScope(
       child: App(),
