8165157895b15f8671bdcee0874930687c57d633
mono <mono0926@gmail.com>
2022-07-08 18:59:27 +0900

    Refactor

--- example/lib/pages/life_cycle_observer_page.dart
+++ example/lib/pages/life_cycle_observer_page.dart
@@ -1,5 +1,3 @@
-import 'dart:async';
-
 import 'package:example/router/router.dart';
 import 'package:example/util/util.dart';
 import 'package:flutter/material.dart';
@@ -13,32 +11,14 @@ class LifeCycleObserverRoute extends GoRouteData {
   Widget build(BuildContext context) => const LifeCycleObserverPage();
 }
 
-class LifeCycleObserverPage extends ConsumerStatefulWidget {
+class LifeCycleObserverPage extends ConsumerWidget {
   const LifeCycleObserverPage({Key? key}) : super(key: key);
 
   @override
-  ConsumerState<LifeCycleObserverPage> createState() =>
-      _LifeCycleObserverPageState();
-}
-
-class _LifeCycleObserverPageState extends ConsumerState<LifeCycleObserverPage> {
-  late final StreamSubscription<void> subscription;
-
-  @override
-  void initState() {
-    super.initState();
-
-    subscription = ref.read(lifecycleObserver.stream).listen(logger.info);
-  }
-
-  @override
-  void dispose() {
-    subscription.cancel();
-    super.dispose();
-  }
-
-  @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
+    ref.listen<AsyncValue<AppLifecycleState>>(lifecycleObserver, (_, state) {
+      logger.info(state.value);
+    });
     return Scaffold(
       appBar: AppBar(
         title: Text(pascalCaseFromRouteName(GoRouter.of(context).location)),
